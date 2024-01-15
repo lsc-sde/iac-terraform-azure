91df645332797a94144165aa4c6aeb3d05d94433
Shaun Turner <Shaun.Turner@headinthecloudsolutions.co.uk>
2024-01-15 16:49:33 +0000

    #30 amended keda config

--- azure/modules/kubernetes-deployment-script/main.tf
+++ azure/modules/kubernetes-deployment-script/main.tf
@@ -53,12 +53,23 @@ resource "azurerm_kubernetes_flux_configuration" "keda" {
     sync_interval_in_seconds = 60
     timeout_in_seconds = 600
   }
-
+  
   kustomizations {
-    name = "keda"
+    name = "cluster-config"
     sync_interval_in_seconds = 60
     retry_interval_in_seconds = 60
     timeout_in_seconds = 600
+    path = "cluster/${var.environment_name}"
+  }
+
+  kustomizations {
+    name = "sources"
+    sync_interval_in_seconds = 60
+    retry_interval_in_seconds = 60
+    timeout_in_seconds = 600
+    path = "sources"
+
+    depends_on = [ "cluster-config" ]
   }
 
   depends_on = [
