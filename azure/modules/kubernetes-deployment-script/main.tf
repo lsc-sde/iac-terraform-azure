f2bcff08805cc04e2e0437b908a1c77cae719ccf
shaun turner <shaun.turner1@nhs.net>
2024-01-16 13:01:12 +0000

    #30 added github-runners flux configuration

--- azure/modules/kubernetes-deployment-script/main.tf
+++ azure/modules/kubernetes-deployment-script/main.tf
@@ -182,3 +182,42 @@ resource "azurerm_kubernetes_flux_configuration" "jupyterhub" {
     azurerm_kubernetes_cluster_extension.flux
   ]
 }
+
+
+
+resource "azurerm_kubernetes_flux_configuration" "github_runner" {
+  name       = "github-runner"
+  cluster_id = var.cluster_id
+  namespace  = "github-runner"
+  scope = "cluster"
+
+  git_repository {
+    url             = "https://github.com/lsc-sde/iac-flux-github-action-runners"
+    reference_type  = "branch"
+    reference_value = var.branch_name
+    sync_interval_in_seconds = 60
+    timeout_in_seconds = 600
+  }
+
+  kustomizations {
+    name = "cluster-config"
+    sync_interval_in_seconds = 60
+    retry_interval_in_seconds = 60
+    timeout_in_seconds = 600
+    path = "cluster/${var.environment_name}"
+  }
+
+  kustomizations {
+    name = "sources"
+    sync_interval_in_seconds = 60
+    retry_interval_in_seconds = 60
+    timeout_in_seconds = 600
+    path = "sources"
+
+    depends_on = [ "cluster-config" ]
+  }
+
+  depends_on = [
+    azurerm_kubernetes_cluster_extension.flux
+  ]
+}
