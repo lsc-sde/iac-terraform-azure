9201e51b4bf8c1d92ff00d02765aa439cff5e9b5
Shaun Turner <shaun.turner1@nhs.net>
2024-02-06 09:59:03 +0000

    fix to kubernetes provider

--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -10,7 +10,7 @@ terraform {
 provider "kubernetes" {
   # Configuration options
   host = var.host
-  client_key = base64encode(var.client_key)
-  client_certificate = base64encode(var.client_certificate)
-  cluster_ca_certificate = base64encode(var.cluster_ca_certificate)
+  client_key = base64decode(var.client_key)
+  client_certificate = base64decode(var.client_certificate)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
