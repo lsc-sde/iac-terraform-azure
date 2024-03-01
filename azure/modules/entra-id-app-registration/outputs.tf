4a976e2942d362be17f948f6a85927f35ebf53a8
Shaun Turner <shaun.turner1@nhs.net>
2024-03-01 16:00:10 +0000

    fix to dependencies

--- azure/modules/entra-id-app-registration/outputs.tf
+++ azure/modules/entra-id-app-registration/outputs.tf
@@ -1,13 +1,13 @@
 output "client_id" {
   value = azuread_application.main.client_id
   depends_on = [ 
-    azuread_service_principal_password.main 
+    azurerm_key_vault_secret.main
     ]
 }
 
 output "object_id" {
   value = azuread_application.main.object_id
   depends_on = [ 
-    azuread_service_principal_password.main 
+    azurerm_key_vault_secret.main
     ]
 }
