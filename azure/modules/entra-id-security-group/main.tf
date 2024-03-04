e5b05965e8341cba3d6acad12f45d4ea27e41f7a
Shaun Turner <shaun.turner1@nhs.net>
2024-03-04 13:46:16 +0000

    not unified type

--- azure/modules/entra-id-security-group/main.tf
+++ azure/modules/entra-id-security-group/main.tf
@@ -1,7 +1,6 @@
 resource "azuread_group" "main" {
   display_name     = local.name
   security_enabled = true
-  types            = ["Unified"]
 
   owners = [
     data.azuread_client_config.current.object_id,
